0890344115cf0791973fc80ad01e50e12f93dafe
mateenOlalekan <MatinOlalek@gmail.com>
2025-11-24 19:50:43 +0100

    i am cooked

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import "./App.css";
 
+import NavbarLogo from "../src/assets/logo.svg";
 // Lazy-loaded pages
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
@@ -29,7 +30,30 @@ const CreateSplitzPage = lazy(() => import("./pages/Dashboard/CreateSplitz"));
 const Notification = lazy(() => import("./pages/Dashboard/Notification"));
 
 // Loading screen
-import Loading from "./loading";
+
+
+
+
+function Loading({ show }) {
+  return (
+    <div
+      className={`fixed inset-0 bg-green-600 flex flex-col items-center justify-center z-[99999]
+        transition-opacity duration-[3s]
+        ${show ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+    >
+      {/* Logo with bounce animation */}
+      <div className="flex justify-center items-center animate-bounce">
+        <img
+          src={NavbarLogo}
+          className="w-20 h-20 md:w-40 md:h-40 drop-shadow-xl"
+          alt="Logo"
+        />
+      </div>
+
+
+    </div>
+  );
+}
 
 export default function App() {
   return (
